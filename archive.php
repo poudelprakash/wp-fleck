32d045daec3dbd2f7a6aef6f80c3ea18be22b20f
prakash <poudelprakash20@gmail.com>
2014-10-05 13:46:51 +0545

    archive page added

---
+++ archive.php
@@ -0,0 +1,41 @@
+<?php get_header( );
+ ?>
+
+<div class="container">
+	<div class="row clearfix">
+		<div class="col-md-9 column">
+			<?php 
+			
+			if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+				<h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+				<p><em><?php the_time('l, F jS, Y'); ?></em></p>
+
+			  	<?php the_excerpt(); ?>
+
+			  	<hr>
+				<?php comments_template(); ?>
+
+			<?php endwhile; else: ?>
+				<p><?php _e('Sorry, this page does not exist.'); ?></p>
+			<?php endif; ?>
+		 
+		</div>
+				<!-- Blog Sidebar Widgets Column -->
+		<div class="col-md-3 column">
+			<div class="well">
+				<?php 
+					//this is for sidebar
+					get_sidebar( ); ?>
+			</div>
+		</div>
+	</div>
+	<hr />
+</div>
+
+
+<?php get_footer( );?>
+
+
+
+  
+  
